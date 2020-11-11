4d90525152039b0bf9a639beb04ac5fed24c19bf
tengfone <phangtf@gmail.com>
2020-11-12 00:42:13 +0800

    Rename automation script

--- scripts/remove.sh
+++ scripts/remove.sh
@@ -1,20 +1,19 @@
 #!/bin/bash
 
-echo "Removing AmaNerdBookReview... =("
+echo "Removing AmaNerdBookReview..."
 sudo apt-get install jq
-echo "Removing Key"
 
 if [ ! -f ./keyData.json ]; then
-    echo "keyData.json not found. No EC2 Instance has been spun"
+    echo "keyData.json not found. No EC2 Instance has been spun. Exiting."
     exit
 else
-    echo "keyData.json found. Removing All keys"
+    echo "keyData.json found. Removing keys..."
     keyName=$(cat keyData.json | jq '.KeyName' | tr -d '"')
-    echo "You have previously used $keyName as a key. Removing from AWS public keypair and deleting local key pair"
+    echo "You have previously used $keyName as a key. Removing from AWS public keypair and deleting local private key pair"
     aws ec2 delete-key-pair --key-name $keyName
     sudo rm key.pem
     sudo rm keyData.json
-    echo "Shutting Down and deleting all EC2 Instances"
+    echo "Shutting Down and removing AmaNerdBookReview Stack from AWS"
     aws cloudformation delete-stack --stack-name AmaNerdBookReview
     echo "AmaNerdBookReview successfully removed! We are sad to see you go, they say a book a day, keep the Cs away!"
 fi
