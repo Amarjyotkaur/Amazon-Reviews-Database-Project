c638bf8bccda12554948c91dd00c729ddad65048
tengfone <phangtf@gmail.com>
2020-11-21 16:28:33 +0800

    second fix mogno

--- scripts/mongo_script/mongo_setup.js
+++ scripts/mongo_script/mongo_setup.js
@@ -1,2 +1,2 @@
-use project
-db.createUser({ user: "admin", pwd: "password", roles: [ { role: "userAdminAnyDatabase", db: "project" }, "readWriteAnyDatabase" ] })
+use admin
+db.createUser({ user: "admin", pwd: "password", roles: ["root"] })
--- test.py
+++
@@ -1,7 +0,0 @@
-fpath = "C:/Users/phang/Desktop/single_note.pkl"
-import pickle
-infile = open(fpath,'rb')
-new_dict = pickle.load(infile)
-infile.close()
-
-print(new_dict)
