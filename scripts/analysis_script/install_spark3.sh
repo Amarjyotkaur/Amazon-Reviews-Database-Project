52732e08d6f50aa94288238c6d1169840fe9e824
tengfone <phangtf@gmail.com>
2020-12-05 00:45:44 +0800

    99% done for script

--- scripts/analysis_script/install_spark3.sh
+++ scripts/analysis_script/install_spark3.sh
@@ -11,8 +11,13 @@ export PATH=$PATH:/opt/hadoop-3.3.0/bin/./
 sudo apt install -y python-pip
 pip install --upgrade setuptools
 python -m pip install --upgrade pip
+pip install pyarrow
+pip install pyspark
 
-df = spark.read.parquet("/user/hadoop/kindle_reviews/3bb93069-ac88-4527-a2f6-1c180e68f2a3.parquet")
+kindleReviewFileName=$(hdfs dfs -stat "%n" /user/hadoop/kindle_reviews/*.parquet)
+
+
+df = spark.read.parquet("/user/hadoop/kindle_reviews/${kindleReviewFileName}")
 df.write.csv("output.csv")
 
 sudo tee ~/tf.py << EOF
@@ -54,26 +59,29 @@ def sparse2dict(vec, idx2word):
     return str({idx2word[i]:v for i,v in zip(idxs, vals)})
 
 def tfidf_review_text(df):
-    with Timer("TF-IDF for reviewText"):
-        df = df.select(["reviewText"]).dropna()
+    try: 
+        with Timer("TF-IDF for reviewText"):
+            df = df.select(["reviewText"]).dropna()
 
-    with Timer("TF-IDF pipeline"):
-        tokenizer = ml.feature.Tokenizer(inputCol="reviewText", outputCol="token")
-        cv = ml.feature.CountVectorizer(inputCol="token", outputCol="hash")
-        idf = ml.feature.IDF(inputCol="hash", outputCol="tfidf")
-        pipeline = ml.Pipeline(stages=[tokenizer, cv, idf])
-        model = pipeline.fit(df)
-        df = model.transform(df)
-    stages = model.stages
-    vectorizers = [s for s in stages if isinstance(s, CountVectorizerModel)]
-    vocab = [v.vocabulary for v in vectorizers]
-    vocab = vocab[0]
-    idx2word = {idx: word for idx, word in enumerate(vocab)}
+        with Timer("TF-IDF pipeline"):
+            tokenizer = ml.feature.Tokenizer(inputCol="reviewText", outputCol="token")
+            cv = ml.feature.CountVectorizer(inputCol="token", outputCol="hash")
+            idf = ml.feature.IDF(inputCol="hash", outputCol="tfidf")
+            pipeline = ml.Pipeline(stages=[tokenizer, cv, idf])
+            model = pipeline.fit(df)
+            df = model.transform(df)
+        stages = model.stages
+        vectorizers = [s for s in stages if isinstance(s, CountVectorizerModel)]
+        vocab = [v.vocabulary for v in vectorizers]
+        vocab = vocab[0]
+        idx2word = {idx: word for idx, word in enumerate(vocab)}
 
-    with Timer("Convert TF-IDF sparseVector to (word:value dict)"):
-        my_udf_func = udf(lambda vector: sparse2dict(vector, idx2word), types.StringType())
-        df = df.select("reviewText", my_udf_func("tfidf").alias("tfidf_final"))
-    show_df(df, 10)
+        with Timer("Convert TF-IDF sparseVector to (word:value dict)"):
+            my_udf_func = udf(lambda vector: sparse2dict(vector, idx2word), types.StringType())
+            df = df.select("reviewText", my_udf_func("tfidf").alias("tfidf_final"))
+        show_df(df, 10)
+    except Exception as e: 
+        print("Misalignment")
     return df
 
 if __name__ == "__main__":
@@ -84,3 +92,8 @@ if __name__ == "__main__":
         df_tfidf = tfidf_review_text(df_reviews)
         df_tfidf.write.format("csv").save("answer/")
 EOF
+
+/opt/spark-3.0.1-bin-hadoop3.2/bin/spark-submit --master yarn  ~/tf.py
+
+# hdfs dfs -get /user/hadoop/answer/part-00005-c785e4dd-06e5-4741-a94a-033b88dc14ed-c000.csv ~/
+# cat part-00005-c785e4dd-06e5-4741-a94a-033b88dc14ed-c000.csv | head -5
--- scripts/main.sh
+++ scripts/main.sh
@@ -176,8 +176,7 @@ function analysis() {
     wait
     # # Start Spark
     ssh -o StrictHostKeyChecking=no ubuntu@${PUBLIC_IPS[3]} -i ./key.pem 'MONGOIP='${PUBLIC_IPS[0]}' MYSQLIP='${PUBLIC_IPS[1]}' WEBSERVERIP='${PUBLIC_IPS[2]}' NAMENODE='${PUBLIC_IPS[3]}' DATANODE1='${PUBLIC_IPS[4]}' DATANODE2='${PUBLIC_IPS[5]}' DATANODE3='${PUBLIC_IPS[6]}' NAMENODEP='${PUBLIC_IPS[7]}' DATANODE1P='${PUBLIC_IPS[8]}' DATANODE2P='${PUBLIC_IPS[9]}' DATANODE3P='${PUBLIC_IPS[10]}' bash -s' <./analysis_script/install_spark2.sh
-    # ssh -o StrictHostKeyChecking=no ubuntu@${PUBLIC_IPS[3]} -i ./key.pem 'MONGOIP='${PUBLIC_IPS[0]}' MYSQLIP='${PUBLIC_IPS[1]}' WEBSERVERIP='${PUBLIC_IPS[2]}' NAMENODE='${PUBLIC_IPS[3]}' DATANODE1='${PUBLIC_IPS[4]}' DATANODE2='${PUBLIC_IPS[5]}' DATANODE3='${PUBLIC_IPS[6]}' NAMENODEP='${PUBLIC_IPS[7]}' DATANODE1P='${PUBLIC_IPS[8]}' DATANODE2P='${PUBLIC_IPS[9]}' DATANODE3P='${PUBLIC_IPS[10]}' bash -s' <./analysis_script/install_spark3.sh
-    
+    ssh -o StrictHostKeyChecking=no ubuntu@${PUBLIC_IPS[3]} -i ./key.pem 'MONGOIP='${PUBLIC_IPS[0]}' MYSQLIP='${PUBLIC_IPS[1]}' WEBSERVERIP='${PUBLIC_IPS[2]}' NAMENODE='${PUBLIC_IPS[3]}' DATANODE1='${PUBLIC_IPS[4]}' DATANODE2='${PUBLIC_IPS[5]}' DATANODE3='${PUBLIC_IPS[6]}' NAMENODEP='${PUBLIC_IPS[7]}' DATANODE1P='${PUBLIC_IPS[8]}' DATANODE2P='${PUBLIC_IPS[9]}' DATANODE3P='${PUBLIC_IPS[10]}' bash -s' <./analysis_script/install_spark3.sh
 }
 
 while getopts ":auhid" OPTION; do
